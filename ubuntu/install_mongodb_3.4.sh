7a2d2ae706b499f5672d5ff8d8a1df3a138a619c
Cristian Balas <cristi8@gmail.com>
2017-01-29 14:13:52 +0200

    Update install_mongodb_3.4.sh

--- ubuntu/install_mongodb_3.4.sh
+++ ubuntu/install_mongodb_3.4.sh
@@ -2,14 +2,10 @@
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-# TODO: Check if 3.3 is the correct value below. At the time of writing, 3.4 was not yet released
-# Check if there is a 3.4 version here: http://repo.mongodb.org/apt/ubuntu/dists/xenial/mongodb-org/
-# Maybe use that instead
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.3 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-aptitude update
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
 
-# TODO: remove "-unstable" after 3.4 is released
-aptitude -y install mongodb-org-unstable
+aptitude update
+aptitude install -y mongodb-org
 
 systemctl start mongod
 systemctl enable mongod
